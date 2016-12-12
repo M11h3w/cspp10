6d8aa8d9bc83fc679c4f403cbda522d636055ecf
Matthew Stewart <matthewstewart@uagateway.org>
2016-12-12 17:15:12 +0000

    Lab

--- unit3/mstewart_craps.py
+++ unit3/mstewart_craps.py
@@ -35,49 +35,53 @@ while player_bank > 0 and casino_bank > 0:
         elif dice_total == 4 or dice_total == 5 or dice_total == 6 or dice_total == 8 or dice_total == 9 or dice_total == 10:
             point_number = dice_total
             print("Your point number is {}".format(point_number))
+            enter = input("Press enter to continue")
             print("")
-            dice1 = random.choice([1,2,3,4,5,6])
-            dice2 = random.choice([1,2,3,4,5,6])
-            dice_total = dice1 + dice2
-            print("The dice is thrown: {} and {}, {} total".format(dice1, dice2, dice_total))
-            if dice_total == 7:
-                casino_bank = casino_bank + bet
-                player_bank = player_bank - bet
-                print("You rolled a 7. You lost the round!")
-                print("You have ${}.".format(player_bank))
-                print("")
-            elif dice_total == point_number:
-                player_bank = player_bank - bet
-                bet = bet * 2
-                player_bank = player_bank + bet
-                print("You rolled your point number! You win the round!")
-                print("You have ${}.".format(player_bank))
-                print("")
-            while dice_total != 7 or dice_total != point_number:
-                print("No 7 or {}. Reroll!".format(point_number))
-                enter = input("Press enter to continue")
-                print("")
-                if enter == "":
-                    dice1 = random.choice([1,2,3,4,5,6])
-                    dice2 = random.choice([1,2,3,4,5,6])
-                    dice_total = dice1 + dice2
-                    print("The dice is thrown: {} and {}, {} total".format(dice1, dice2, dice_total))
-                    if dice_total == 7:
-                        casino_bank = casino_bank + bet
-                        player_bank = player_bank - bet
-                        print("You rolled a 7. You lost the round!")
-                        print("You have ${}.".format(player_bank))
+            if enter == "":
+                dice1 = random.choice([1,2,3,4,5,6])
+                dice2 = random.choice([1,2,3,4,5,6])
+                dice_total = dice1 + dice2
+                print("The dice is thrown: {} and {}, {} total".format(dice1, dice2, dice_total))
+                if dice_total == 7:
+                    dice_total == 7
+                    casino_bank = casino_bank + bet
+                    player_bank = player_bank - bet
+                    print("You rolled a 7. You lost the round!")
+                    print("You have ${}.".format(player_bank))
+                    print("")
+                elif dice_total == point_number:
+                    dice_total == point_number
+                    player_bank = player_bank - bet
+                    bet = bet * 2
+                    player_bank = player_bank + bet
+                    print("You rolled your point number! You win the round!")
+                    print("You have ${}.".format(player_bank))
+                    print("")
+                else:
+                    while dice_total != 7 or dice_total != point_number:
+                        print("No 7 or {}. Reroll!".format(point_number))
+                        enter = input("Press enter to continue")
                         print("")
-                        break
-                    elif dice_total == point_number:
-                        player_bank = player_bank - bet
-                        bet = bet * 2
-                        player_bank = player_bank + bet
-                        print("You rolled your point number! You win the round!")
-                        print("You have ${}.".format(player_bank))
-                        print("")
-                        break
-    if casino_bank == 0:
+                        if enter == "":
+                            dice1 = random.choice([1,2,3,4,5,6])
+                            dice2 = random.choice([1,2,3,4,5,6])
+                            dice_total = dice1 + dice2
+                            print("The dice is thrown: {} and {}, {} total".format(dice1, dice2, dice_total))
+                            if dice_total == 7:
+                                casino_bank = casino_bank + bet
+                                player_bank = player_bank - bet
+                                print("You rolled a 7. You lost the round!")
+                                print("You have ${}.".format(player_bank))
+                                print("")
+                                break
+                            elif dice_total == point_number:
+                                player_bank = player_bank - bet
+                                bet = bet * 2
+                                player_bank = player_bank + bet
+                                print("You rolled your point number! You win the round!")
+                                print("You have ${}.".format(player_bank))
+                                print("")
+                                break
+    elif casino_bank == 0:
         print("You beat the casino!")
         break
-            
