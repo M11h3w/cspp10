241830ecf762259eca942df8c604cf6d43139e82
Matthew Stewart <matthewstewart@uagateway.org>
2017-02-02 17:13:24 +0000

    Lab

--- unit5/mstewart_scramble.py
+++ unit5/mstewart_scramble.py
@@ -1,5 +1,12 @@
+import random
 word = input()
-if len(word) == 4:
-    dont_scarmble1 = word[0]
-    dont_scramble2 = word[3]
-    word.shuffle()
+# if len(word) == 4:
+#     dont_scarmble1 = word[0]
+#     dont_scramble2 = word[3]
+# elif len(word) > 4:
+#     word_l(word)
+#     word.split(" ")
+#     print(word)
+word.split(" ")
+random.shuffle(list(word))
+print(word)
