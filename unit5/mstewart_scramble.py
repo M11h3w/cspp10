66f1cbc16484e8834dbb656679f5b26e7bee6aab
Matthew Stewart <matthewstewart@uagateway.org>
2017-02-06 17:16:12 +0000

    Worked on word scrambler

--- unit5/mstewart_scramble.py
+++ unit5/mstewart_scramble.py
@@ -5,9 +5,8 @@ last = word[-1]
 if len(word) == 4:
     word2 = word[1:3]
     s = list(word2)
-    random.shuffle(s)
+    s2 = s
+    while s2 == s:
+        s2 = random.shuffle(s)
     # print(word2)
-    print(s)
-# s = [1,2,3,4]
-# random.shuffle(s)
-# print(s)
+    print(s2)
